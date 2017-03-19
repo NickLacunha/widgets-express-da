1be129297babf3ea0935a191194972632697187f
NickLacunha <njpadinha@gmail.com>
2017-03-18 22:47:51 -0700

    Dummy page
    Customers links to a dummy page powere by angular now

--- WidgetsExpressDesktop/app/app.js
+++ WidgetsExpressDesktop/app/app.js
@@ -3,7 +3,8 @@
 // Declare app level module which depends on views, and components
 angular.module('desktopApp', [
   'ngRoute',
-  'desktopApp.home'
+  'desktopApp.home',
+  'desktopApp.customers'
 ]).
 config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
   $locationProvider.hashPrefix('!');
---
+++ WidgetsExpressDesktop/app/customers/customers.js
@@ -0,0 +1,14 @@
+'use strict';
+
+angular.module('desktopApp.customers', ['ngRoute'])
+
+.config(['$routeProvider', function($routeProvider) {
+  $routeProvider.when('/customers', {
+    templateUrl: 'customers/customers.html',
+    controller: 'customersControl'
+  });
+}])
+
+.controller('View1Control', [function() {
+
+}]);
